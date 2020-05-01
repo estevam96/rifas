d71c050c41dd49b2892af68783342fee71808449
estevam96 <marcelo.estevam15@hotmail.com>
2020-05-01 13:13:13 -0300

    criado migration e model Payment

---
+++ database/migrations/2020_05_01_154522_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('image', 50);
+            $table->string('bank', 80);
+            $table->string('holder', 100);
+            $table->string('cpf', 11);
+            $table->string('agency', 80);
+            $table->string('account', 80);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
