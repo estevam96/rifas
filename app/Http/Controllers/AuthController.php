d432320ec6275098585dc35bb53fb3f07e8a283e
estevam96 <marcelo.estevam15@hotmail.com>
2020-06-06 10:59:22 -0300

    add Request parament in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-  public function login()
+  public function login(Request $request)
   {
-    $credentials = request(['email', 'password']);
+    $credentials = $request->only(['email', 'password']);
 
     if (!$token = auth('api')->attempt($credentials)) {
       return response()->json(['error' => 'Unauthorized'], 401);
